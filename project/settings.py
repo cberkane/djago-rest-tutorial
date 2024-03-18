72a8abcd1b3b14b77d4d02c69540321bb0cf9988
chihab <c.berkane@hotmail.fr>
2024-03-18 23:49:35 +0100

    WIP

--- project/settings.py
+++ project/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'shop',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
